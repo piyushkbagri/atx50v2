5137760a8c7ae54d2c0c52aca61c99b35a63515d
test2 dev2source <test2.dev2source@gmail.com>
2014-05-21 03:59:14 -0500

    Changes made by test2 dev2source (test2.dev2source@gmail.com).

--- Ask Science Experts/scripts/application.js
+++ Ask Science Experts/scripts/application.js
@@ -57,6 +57,7 @@ var Application = {
         
             .on('pageinit', '#atx-form-page', function () {
                 qSubmit = false;
+                $('[type="submit"]').button('enable');
                 Application._getDbValues();
                 $('#atx-country').selectmenu("refresh");
                 //$('#atx-age').selectmenu("refresh");
@@ -123,19 +124,25 @@ var Application = {
         //Question submit step 1
         //$(document).off("submit", '#atx-questionform').on("submit", '#atx-questionform', function(event) {       
        $('#atx-questionform').submit(function (event) {
+            
+            $( '#atx-questionform [type="submit"]').button('disable');
             var data = { };
             data['question'] = $('#atx-question').val().trim(); 
             data['category'] = $('#atx-question-category').val().trim();  
             if (data['category'] === '') {
                 navigator.notification.alert('Invalid Category, Please select question category', function () {
                 }, 'Error');
+                 $('[type="submit"]').button('enable');
                 return false;
             }              
             if (data['question'] === '') {
                 navigator.notification.alert('Question is required and cannot be empty', function () {
                 }, 'Error');
+                 $('[type="submit"]').button('enable');
                 return false;
             }
+           
+         //   $('[type="submit"]').button('enable');
             event.preventDefault();
             $.mobile.changePage("#atx-form-page", { transition: "none", changeHash: true });
             return false;
@@ -150,8 +157,10 @@ var Application = {
         	jsonRequest = null;
             $.mobile.loading('hide');
             qSubmit = false;
-            ajaxing = false;
+         //   $('[type="submit"]').button('enable');
+            ajaxing = false;    
            } 
+            $('[type="submit"]').button('enable'); 
            
         });
         
@@ -176,7 +185,7 @@ var Application = {
 
             event.preventDefault();
             var data = Application._setDbValues();
-            console.log(data);
+           // console.log(data);
             if (typeof(data) == 'boolean')
                 return false;
             
@@ -184,7 +193,8 @@ var Application = {
                 navigator.notification.alert('Submitting is in progress', function () {
                 }, 'Error');
                 return;
-            }    
+            } 
+            
             qSubmit = true;     
             qSubmitSuccess = false;
             var url = $server + 'services/atx_tktsubmit';
@@ -214,6 +224,7 @@ var Application = {
                        complete: function () {
                            $.mobile.loading('hide');
                            qSubmit = false;
+                           $('[type="submit"]').button('enable');
                            console.log('Question submitted:ajax Complete');
                            if (qSubmitSuccess == true) {
                                $.mobile.changePage("#atx-success-page", { transition: "fade", changeHash: false });
@@ -222,6 +233,7 @@ var Application = {
 
                        beforeSend: function() {
                            $.mobile.loading('show');
+                           $('#atx-form [type="submit"]').button('disable');
                        },
                       error: function (e, textStatus) {
                           Application.showError (e, textStatus);
@@ -631,7 +643,7 @@ var Application = {
     },
     
     loadCountry: function() {
-        var country = {"AF" : "Afghanistan", "AL" : "Albania", "DZ" : "Algeria", "AS" : "American Samoa", "AD" : "Andorra", "AO" : "Angola", "AI" : "Anguilla", "AQ" : "Antarctica", "AG" : "Antigua and Barbuda", "AR" : "Argentina", "AM" : "Armenia", "AW" : "Aruba", "AU" : "Australia", "AT" : "Austria", "AZ" : "Azerbaijan", "BS" : "Bahamas", "BH" : "Bahrain", "BD" : "Bangladesh", "BB" : "Barbados", "BY" : "Belarus", "BE" : "Belgium", "BZ" : "Belize", "BJ" : "Benin", "BM" : "Bermuda", "BT" : "Bhutan", "BO" : "Bolivia", "BA" : "Bosnia and Herzegovina", "BW" : "Botswana", "BV" : "Bouvet Island", "BR" : "Brazil", "IO" : "British Indian Ocean Territory", "BN" : "Brunei Darussalam", "BG" : "Bulgaria", "BF" : "Burkina Faso", "BI" : "Burundi", "KH" : "Cambodia", "CM" : "Cameroon", "CA" : "Canada", "CV" : "Cape Verde", "KY" : "Cayman Islands", "CF" : "Central African Republic", "TD" : "Chad", "CL" : "Chile", "CN" : "China", "CX" : "Christmas Island", "CC" : "Cocos (Keeling) Islands", "CO" : "Colombia", "KM" : "Comoros", "CG" : "Congo", "CD" : "Congo, the Democratic Republic of the", "CK" : "Cook Islands", "CR" : "Costa Rica", "CI" : "Cote D'Ivoire", "HR" : "Croatia", "CU" : "Cuba", "CY" : "Cyprus", "CZ" : "Czech Republic", "DK" : "Denmark", "DJ" : "Djibouti", "DM" : "Dominica", "DO" : "Dominican Republic", "EC" : "Ecuador", "EG" : "Egypt", "SV" : "El Salvador", "GQ" : "Equatorial Guinea", "ER" : "Eritrea", "EE" : "Estonia", "ET" : "Ethiopia", "FK" : "Falkland Islands (Malvinas)", "FO" : "Faroe Islands", "FJ" : "Fiji", "FI" : "Finland", "FR" : "France", "GF" : "French Guiana", "PF" : "French Polynesia", "TF" : "French Southern Territories", "GA" : "Gabon", "GM" : "Gambia", "GE" : "Georgia", "DE" : "Germany", "GH" : "Ghana", "GI" : "Gibraltar", "GR" : "Greece", "GL" : "Greenland", "GD" : "Grenada", "GP" : "Guadeloupe", "GU" : "Guam", "GT" : "Guatemala", "GN" : "Guinea", "GW" : "Guinea-Bissau", "GY" : "Guyana", "HT" : "Haiti", "HM" : "Heard Island and Mcdonald Islands", "VA" : "Holy See (Vatican City State)", "HN" : "Honduras", "HK" : "Hong Kong", "HU" : "Hungary", "IS" : "Iceland", "IN" : "India", "ID" : "Indonesia", "IR" : "Iran, Islamic Republic of", "IQ" : "Iraq", "IE" : "Ireland", "IL" : "Israel", "IT" : "Italy", "JM" : "Jamaica", "JP" : "Japan", "JO" : "Jordan", "KZ" : "Kazakhstan", "KE" : "Kenya", "KI" : "Kiribati", "KP" : "Korea, Democratic People's Republic of", "KR" : "Korea, Republic of", "KW" : "Kuwait", "KG" : "Kyrgyzstan", "LA" : "Lao People's Democratic Republic", "LV" : "Latvia", "LB" : "Lebanon", "LS" : "Lesotho", "LR" : "Liberia", "LY" : "Libyan Arab Jamahiriya", "LI" : "Liechtenstein", "LT" : "Lithuania", "LU" : "Luxembourg", "MO" : "Macao", "MK" : "Macedonia, the Former Yugoslav Republic of", "MG" : "Madagascar", "MW" : "Malawi", "MY" : "Malaysia", "MV" : "Maldives", "ML" : "Mali", "MT" : "Malta", "MH" : "Marshall Islands", "MQ" : "Martinique", "MR" : "Mauritania", "MU" : "Mauritius", "YT" : "Mayotte", "MX" : "Mexico", "FM" : "Micronesia, Federated States of", "MD" : "Moldova, Republic of", "MC" : "Monaco", "MN" : "Mongolia", "MS" : "Montserrat", "MA" : "Morocco", "MZ" : "Mozambique", "MM" : "Myanmar", "NA" : "Namibia", "NR" : "Nauru", "NP" : "Nepal", "NL" : "Netherlands", "AN" : "Netherlands Antilles", "NC" : "New Caledonia", "NZ" : "New Zealand", "NI" : "Nicaragua", "NE" : "Niger", "NG" : "Nigeria", "NU" : "Niue", "NF" : "Norfolk Island", "MP" : "Northern Mariana Islands", "NO" : "Norway", "OM" : "Oman", "PK" : "Pakistan", "PW" : "Palau", "PS" : "Palestinian Territory, Occupied", "PA" : "Panama", "PG" : "Papua New Guinea", "PY" : "Paraguay", "PE" : "Peru", "PH" : "Philippines", "PN" : "Pitcairn", "PL" : "Poland", "PT" : "Portugal", "PR" : "Puerto Rico", "QA" : "Qatar", "RE" : "Reunion", "RO" : "Romania", "RU" : "Russian Federation", "RW" : "Rwanda", "SH" : "Saint Helena", "KN" : "Saint Kitts and Nevis", "LC" : "Saint Lucia", "PM" : "Saint Pierre and Miquelon", "VC" : "Saint Vincent and the Grenadines", "WS" : "Samoa", "SM" : "San Marino", "ST" : "Sao Tome and Principe", "SA" : "Saudi Arabia", "SN" : "Senegal", "CS" : "Serbia and Montenegro", "SC" : "Seychelles", "SL" : "Sierra Leone", "SG" : "Singapore", "SK" : "Slovakia", "SI" : "Slovenia", "SB" : "Solomon Islands", "SO" : "Somalia", "ZA" : "South Africa", "GS" : "South Georgia and the South Sandwich Islands", "ES" : "Spain", "LK" : "Sri Lanka", "SD" : "Sudan", "SR" : "Suriname", "SJ" : "Svalbard and Jan Mayen", "SZ" : "Swaziland", "SE" : "Sweden", "CH" : "Switzerland", "SY" : "Syrian Arab Republic", "TW" : "Taiwan, Province of China", "TJ" : "Tajikistan", "TZ" : "Tanzania, United Republic of", "TH" : "Thailand", "TL" : "Timor-Leste", "TG" : "Togo", "TK" : "Tokelau", "TO" : "Tonga", "TT" : "Trinidad and Tobago", "TN" : "Tunisia", "TR" : "Turkey", "TM" : "Turkmenistan", "TC" : "Turks and Caicos Islands", "TV" : "Tuvalu", "UG" : "Uganda", "UA" : "Ukraine", "AE" : "United Arab Emirates", "GB" : "United Kingdom", "US" : "United States", "UM" : "United States Minor Outlying Islands", "UY" : "Uruguay", "UZ" : "Uzbekistan", "VU" : "Vanuatu", "VE" : "Venezuela", "VN" : "Viet Nam", "VG" : "Virgin Islands, British", "VI" : "Virgin Islands, U.s.", "WF" : "Wallis and Futuna", "EH" : "Western Sahara", "YE" : "Yemen", "ZM" : "Zambia", "ZW" : "Zimbabwe" };
+        var country = {"AF" : "Afghanistan", "AL" : "Albania", "DZ" : "Algeria", "AS" : "American Samoa", "AD" : "Andorra", "AO" : "Angola", "AI" : "Anguilla", "AQ" : "Antarctica", "AG" : "Antigua and Barbuda", "AR" : "Argentina", "AM" : "Armenia", "AW" : "Aruba", "AU" : "Australia", "AT" : "Austria", "AZ" : "Azerbaijan", "BS" : "Bahamas", "BH" : "Bahrain", "BD" : "Bangladesh", "BB" : "Barbados", "BY" : "Belarus", "BE" : "Belgium", "BZ" : "Belize", "BJ" : "Benin", "BM" : "Bermuda", "BT" : "Bhutan", "BO" : "Bolivia", "BA" : "Bosnia and Herzegovina", "BW" : "Botswana", "BV" : "Bouvet Island", "BR" : "Brazil", "IO" : "British Indian Ocean Territory", "BN" : "Brunei Darussalam", "BG" : "Bulgaria", "BF" : "Burkina Faso", "BI" : "Burundi", "KH" : "Cambodia", "CM" : "Cameroon", "CA" : "Canada", "CV" : "Cape Verde", "KY" : "Cayman Islands", "CF" : "Central African Republic", "TD" : "Chad", "CL" : "Chile", "CN" : "China", "CX" : "Christmas Island", "CC" : "Cocos (Keeling) Islands", "CO" : "Colombia", "KM" : "Comoros", "CG" : "Congo", "CD" : "Congo, the Democratic Republic of the", "CK" : "Cook Islands", "CR" : "Costa Rica", "CI" : "Cote D'Ivoire", "HR" : "Croatia", "CU" : "Cuba", "CY" : "Cyprus", "CZ" : "Czech Republic", "DK" : "Denmark", "DJ" : "Djibouti", "DM" : "Dominica", "DO" : "Dominican Republic", "EC" : "Ecuador", "EG" : "Egypt", "SV" : "El Salvador", "GQ" : "Equatorial Guinea", "ER" : "Eritrea", "EE" : "Estonia", "ET" : "Ethiopia", "FK" : "Falkland Islands (Malvinas)", "FO" : "Faroe Islands", "FJ" : "Fiji", "FI" : "Finland", "FR" : "France", "GF" : "French Guiana", "PF" : "French Polynesia", "TF" : "French Southern Territories", "GA" : "Gabon", "GM" : "Gambia", "GE" : "Georgia", "DE" : "Germany", "GH" : "Ghana", "GI" : "Gibraltar", "GR" : "Greece", "GL" : "Greenland", "GD" : "Grenada", "GP" : "Guadeloupe", "GU" : "Guam", "GT" : "Guatemala", "GN" : "Guinea", "GW" : "Guinea-Bissau", "GY" : "Guyana", "HT" : "Haiti", "HM" : "Heard Island and Mcdonald Islands", "VA" : "Holy See (Vatican City State)", "HN" : "Honduras", "HK" : "Hong Kong", "HU" : "Hungary", "IS" : "Iceland", "IN" : "India", "ID" : "Indonesia", "IR" : "Iran, Islamic Republic of", "IQ" : "Iraq", "IE" : "Ireland", "IL" : "Israel", "IT" : "Italy", "JM" : "Jamaica", "JP" : "Japan", "JO" : "Jordan", "KZ" : "Kazakhstan", "KE" : "Kenya", "KI" : "Kiribati", "KP" : "Korea, Democratic People's Republic of", "KR" : "Korea, Republic of", "KW" : "Kuwait", "KG" : "Kyrgyzstan", "LA" : "Lao People's Democratic Republic", "LV" : "Latvia", "LB" : "Lebanon", "LS" : "Lesotho", "LR" : "Liberia", "LY" : "Libyan Arab Jamahiriya", "LI" : "Liechtenstein", "LT" : "Lithuania", "LU" : "Luxembourg", "MO" : "Macao", "MK" : "Macedonia, the Former Yugoslav Republic of", "MG" : "Madagascar", "MW" : "Malawi", "MY" : "Malaysia", "MV" : "Maldives", "ML" : "Mali", "MT" : "Malta", "MH" : "Marshall Islands", "MQ" : "Martinique", "MR" : "Mauritania", "MU" : "Mauritius", "YT" : "Mayotte", "MX" : "Mexico", "FM" : "Micronesia, Federated States of", "MD" : "Moldova, Republic of", "MC" : "Monaco", "MN" : "Mongolia", "MS" : "Montserrat", "MA" : "Morocco", "MZ" : "Mozambique", "MM" : "Myanmar", "NA" : "Namibia", "NR" : "Nauru", "NP" : "Nepal", "NL" : "Netherlands", "AN" : "Netherlands Antilles", "NC" : "New Caledonia", "NZ" : "New Zealand", "NI" : "Nicaragua", "NE" : "Niger", "NG" : "Nigeria", "NU" : "Niue", "NF" : "Norfolk Island", "MP" : "Northern Mariana Islands", "NO" : "Norway", "OM" : "Oman", "PK" : "Pakistan", "PW" : "Palau", "PS" : "Palestinian Territory, Occupied", "PA" : "Panama", "PG" : "Papua New Guinea", "PY" : "Paraguay", "PE" : "Peru", "PH" : "Philippines", "PN" : "Pitcairn", "PL" : "Poland", "PT" : "Portugal", "PR" : "Puerto Rico", "QA" : "Qatar", "RE" : "Reunion", "RO" : "Romania", "RU" : "Russian Federation", "RW" : "Rwanda", "SH" : "Saint Helena", "KN" : "Saint Kitts and Nevis", "LC" : "Saint Lucia", "PM" : "Saint Pierre and Miquelon", "VC" : "Saint Vincent and the Grenadines", "WS" : "Samoa", "SM" : "San Marino", "ST" : "Sao Tome and Principe", "SA" : "Saudi Arabia", "SN" : "Senegal", "CS" : "Serbia and Montenegro", "SC" : "Seychelles", "SL" : "Sierra Leone", "SG" : "Singapore", "SK" : "Slovakia", "SI" : "Slovenia", "SB" : "Solomon Islands", "SO" : "Somalia", "ZA" : "South Africa", "GS" : "South Georgia and the South Sandwich Islands", "ES" : "Spain", "LK" : "Sri Lanka", "SD" : "Sudan", "SR" : "Suriname", "SJ" : "Svalbard and Jan Mayen", "SZ" : "Swaziland", "SE" : "Sweden", "CH" : "Switzerland", "SY" : "Syrian Arab Republic", "TW" : "Taiwan, Province of China", "TJ" : "Tajikistan", "TZ" : "Tanzania, United Republic of", "TH" : "Thailand", "TL" : "Timor-Leste", "TG" : "Togo", "TK" : "Tokelau", "TO" : "Tonga", "TT" : "Trinidad and Tobago", "TN" : "Tunisia", "TR" : "Turkey", "TM" : "Turkmenistan", "TC" : "Turks and Caicos Islands", "TV" : "Tuvalu", "UG" : "Uganda", "UA" : "Ukraine", "AE" : "United Arab Emirates", "UK" : "United Kingdom", "US" : "United States", "UM" : "United States Minor Outlying Islands", "UY" : "Uruguay", "UZ" : "Uzbekistan", "VU" : "Vanuatu", "VE" : "Venezuela", "VN" : "Viet Nam", "VG" : "Virgin Islands, British", "VI" : "Virgin Islands, U.s.", "WF" : "Wallis and Futuna", "EH" : "Western Sahara", "YE" : "Yemen", "ZM" : "Zambia", "ZW" : "Zimbabwe" };
         var str = '';
         for (var c in country) {
             str += '<option value="' + c + '">' + country[c] + '</option>';
@@ -668,6 +680,10 @@ var Application = {
                 navigator.notification.alert('Name field is required and cannot be empty', function () { }, 'Error');
                 return false;
             }
+            if (!Application.validateName(data['name'])) {
+                navigator.notification.alert('Invalid Name', function () { }, 'Error');
+                return false;
+            } 
             if (data['age'] === '' ) {
                 navigator.notification.alert('Invalid Age, Please select your age', function () { }, 'Error');
                 return false;
@@ -723,6 +739,12 @@ var Application = {
                 alert('Name field is required and cannot be empty');
                 return false;
             }
+                     
+  			if (!Application.validateName(data['name'])) {
+                alert('Invalid Name');
+                return false;
+            } 
+            
             if (data['country'] === '') {
                 alert('Invalid Country, Please select your country');
                 return false;
@@ -735,7 +757,7 @@ var Application = {
                 alert('Invalid email address');
                 return false;
             }
-
+       
             if (data['age'] === '') {
                 alert('Invalid Age, Please select your age');
                 return false;
@@ -757,6 +779,7 @@ var Application = {
             if (items.name != undefined) {
                 $("#atx-name").val(items.name);
             }
+            
             if (items.email != undefined) {
                 result = document.getElementById("atx-email");
                 result.value = items.email;
@@ -798,6 +821,16 @@ var Application = {
         return true;
     } ,
 
+   validateName: function (name) {
+        var reg = /^[a-zA-Z][a-zA-Z0-9-_\.\s]{1,25}$/;
+
+        if (reg.test(name) == false) {
+            return false;
+        }
+ 
+        return true;
+    } ,
+    
 };
 
 // create seasons array and and repeat ajax call until all results are returned
